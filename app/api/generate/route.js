f4b4ab9c14d58811d71343dd935732f6858feb95
rhileyyy <rhiley.scholes123@gmail.com>
2026-04-30 09:10:28 +1000

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -415,7 +415,7 @@ export async function POST(request) {
         responseModalities: ['TEXT', 'IMAGE'],
         imageConfig: {
           aspectRatio: '1:1',
-          imageSize: '512',
+          imageSize: '1k',
         },
       },
     });
